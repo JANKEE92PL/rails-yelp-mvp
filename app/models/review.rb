61f5c04946a44e1a03d10d5b766d98766576ea81
JANKEE92PL <jankee112@freenet.de>
2022-01-18 23:34:34 +0100

    add validations to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   validates :rating, presence: true, inclusion: { in: rand(1..5), message: "%{value} is not a valid size" }
+  validates :content, presence: true
+  belongs_to :restaurant
 end
