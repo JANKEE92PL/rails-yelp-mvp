1c022079cbac87703237e19df35050b92086ce90
JANKEE92PL <jankee112@freenet.de>
2022-01-19 12:34:19 +0100

    migartion

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning up database"
+Restaurant.destroy_all
+puts "database is clean"
+
+puts "Creating Restaurants"
+100.times do
+  Restaurant.create(
+    name: FAKER::Restaurant.name,
+    address: FAKER::Address.street_address,
+    rating: rand(1..5)
+  )
+end
